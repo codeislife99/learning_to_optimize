6f3a3fa994af52560bf68a68fd2410613f97eca8
Junru Shao <junrushao1994@gmail.com>
2018-03-19 03:55:49 -0400

    Add SimpleNeuralNetwork Env

--- environment.py
+++ environment.py
@@ -1,10 +1,15 @@
-from sklearn.datasets import make_classification
-from scipy.linalg import qr
-import numpy as np
+import argparse
 import sys
 import time
-import argparse
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
 from numpy.linalg import inv
+from scipy.linalg import qr
+from sklearn.datasets import make_classification
+
 
 class QuadraticEnvironment(object):
 
@@ -126,3 +131,95 @@ class LogisticEnvironment(object):
         reward = prev_func_val - func_val
         self.func_val = func_val
         return np.hstack((self.current_iterate, self.gradient, np.clip(np.expand_dims(self.func_val,1), -1e4, 1e4))), reward
+
+
+class _MLP(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_dim, n_layers):
+        super(_MLP, self).__init__()
+        last_dim = input_dim
+        layers = []
+        for _ in range(n_layers - 1):
+            layers.append(nn.Linear(last_dim, hidden_dim, bias=True))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_dim, output_dim))
+        self.layers = nn.Sequential(*layers)
+        self.param_pos = _MLP.get_param_pos(self)
+
+    def forward(self, data_x):
+        return self.layers(data_x)
+
+    def get_loss(self, data_x, data_y):
+        logits = self.__call__(data_x)
+        result = nn.functional.cross_entropy(logits, data_y)
+        return result
+
+    def get_weights(self):
+        return _MLP.to_param_vector(self, self.param_pos, use_grad=False)
+
+    def get_grad(self):
+        return _MLP.to_param_vector(self, self.param_pos, use_grad=True)
+
+    @staticmethod
+    def get_param_pos(model: nn.Module):
+        pos = {}
+        last_pos = 0
+        for name, param in model.named_parameters():
+            length = param.data.view(-1).size(0)
+            pos[name] = (last_pos, last_pos + length)
+            last_pos += length
+        assert "len" not in pos
+        pos["len"] = last_pos
+        return pos
+
+    @staticmethod
+    def to_param_vector(model: nn.Module, pos: dict, use_grad):
+        res = np.zeros(pos["len"], dtype="float32")
+        for name, param in model.named_parameters():
+            if use_grad:
+                param = param.grad.data.view(-1)
+            else:
+                param = param.data.view(-1)
+            st_pos, ed_pos = pos[name]
+            res[st_pos: ed_pos] = param.cpu().numpy()
+        res = torch.from_numpy(res)
+        return res
+
+
+class SimpleNeuralNetwork(object):
+
+    def __init__(self, batch_size, dimensions, n_samples=100, hidden_dim=10):
+        # TODO: definition of dimension
+        from dataset import get_synthetic
+        data_x, data_y = get_synthetic(n_samples, dimensions, n_classes=2)
+        self.data_x = torch.from_numpy(data_x.astype("float32"))
+        self.data_y = torch.from_numpy(data_y.astype("int64"))
+        self.batch_size = batch_size
+        self.dimensions = dimensions
+        self.hidden_dim = hidden_dim
+        self.reset_state()
+
+    def get_state(self):
+        return np.hstack((self.current_iterate, self.gradient, np.clip(np.expand_dims(self.func_val, 1), -1e4, 1e4)))
+
+    def reset_state(self):
+        data_x = Variable(self.data_x, volatile=False)
+        data_y = Variable(self.data_y, volatile=False)
+        self.model = _MLP(self.dimensions, self.hidden_dim, 2, 3)
+        self.model.zero_grad()
+        loss = self.model.get_loss(data_x, data_y)
+        loss.backward()
+        self.current_iterate = self.model.get_weights()
+        self.gradient = self.model.get_grad()
+        self.func_val = loss.data[0]
+
+    def __call__(self, step_size):
+        data_x = Variable(self.data_x, volatile=False)
+        data_y = Variable(self.data_y, volatile=False)
+        self.model.zero_grad()
+        loss = self.model.get_loss(data_x, data_y)
+        loss.backward()
+        for param in self.model.parameters():
+            if param.grad is None:
+                continue
+            param.data.add_(-step_size, param.grad.data)
