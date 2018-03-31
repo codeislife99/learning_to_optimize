e790c40c92ce43f369d9c6ac77f25f9b03eef56a
Ritwik Das <ritwikdas54@gmail.com>
2018-03-30 23:34:58 -0400

    Added LR Scheduler on Plateau

--- l2o/train_classical.py
+++ l2o/train_classical.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 from pylab import *
 import argparse
 
-batch_size = 10
-dimension = 5
+batch_size = 256
+dimension = 100
 quad_env = QuadraticEnvironment(batch_size,dimension)
 iter_arr = []
 reward_arr = []
@@ -21,12 +21,20 @@ diff_to_optim_val_arr = []
 diff_to_optim_x_squared_arr = []
 function_val_arr = []
 x = Variable(torch.zeros(batch_size, dimension), requires_grad=True)      
-optimizer = optim.Adam([{'params': x}],lr = 0.01)
-print(quad_env.x_opt)
-for iter in range(1000000):
-	print("X = ", x.data.cpu().numpy())
-	print("Opti X = ", quad_env.x_opt)
+optimizer = optim.Adam([{'params': x}],lr = 0.1)
+
+# scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode = 'min', threshold = 0.1,min_lr = 0.1)
+for iter in range(100000):
+	if iter%100==0:
+		current_val = x.data.cpu().numpy()
+		opti_val = quad_env.x_opt
+		dist = np.linalg.norm(current_val - opti_val)
+		print("Dist = ", dist)
+		# print("X = ", current_val)
+		# print("Opti X = ", opti_val)
 	result = quad_env(x)
 	optimizer.zero_grad()
 	result.sum().backward()
 	optimizer.step()
+	# dist = np.linalg.norm(x.data.cpu().numpy() - quad_env.x_opt)
+	# scheduler.step(dist)
