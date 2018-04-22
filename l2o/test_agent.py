2dd338562d3fd691facf506325bdb63470f26076
Yang Zhang <yz6@andrew.cmu.edu>
2018-04-22 15:41:06 -0400

    test: uses more base optimizers good for plotting

--- l2o/test_agent.py
+++ l2o/test_agent.py
@@ -38,48 +38,74 @@ def test(meta_model_dir):
     meta_rewards = np.asarray(meta_rewards)
     avg_meta_func_vals = np.mean(meta_func_vals, axis=1)
     avg_meta_rewards = np.mean(meta_rewards, axis=1)
-    env.reset()
-    env.cuda()
 
-    if args.optim_base == 'adam':
-        base_optimizer = optim.Adam(env.all_params, lr=args.lr_base, eps=1e-5)
-    elif args.optim_base == 'sgd':
-        base_optimizer = optim.SGD(env.all_params, lr=args.lr_base)
+    std_meta_func_vals = np.std(meta_func_vals, axis=1)
+    std_meta_rewards = np.std(meta_rewards, axis=1)
 
-    base_func_vals, base_rewards = [], []
-    # import ipdb; ipdb.set_trace()
-    for step in trange(args.n_steps_test):
-        base_optimizer.zero_grad()
-        prev_func_vals = env.func_val
-        next_func_vals = env._eval()
-        base_optimizer.step()
-        env.func_val = next_func_vals
-        reward = prev_func_vals - next_func_vals
-        base_func_vals.append(_to_cpu(next_func_vals))
-        base_rewards.append(_to_cpu(reward))
-    base_func_vals = np.asarray(base_func_vals)
-    base_rewards = np.asarray(base_rewards)
+    print(f'saving meta test data...')
+    np.save(f'{meta_model_dir}/avg_meta_func_vals.npy', avg_meta_func_vals)
+    np.save(f'{meta_model_dir}/std_meta_func_vals.npy', std_meta_func_vals)
+    np.save(f'{meta_model_dir}/meta_func_vals.npy', np.transpose(meta_func_vals))
 
-    avg_base_func_vals = np.mean(base_func_vals, axis=1)
-    avg_base_rewards = np.mean(base_rewards, axis=1)
-
-    data_x = np.arange(args.n_steps_test)
-    fig_func_val = plot_data(data_x=data_x, data_y=avg_meta_func_vals, label_x='steps', label_y='Func Val', label='meta', fig_no=0)
-    fig_func_val = plot_data(data_x=data_x, data_y=avg_base_func_vals, label_x='steps', label_y='Func Val', label=f'{args.optim_base}', fig_no=0)
-
-
-    fig_rewards = plot_data(data_x=data_x, data_y=avg_meta_rewards, label_x='steps', label_y='rewards', label='meta', fig_no=1)
-    fig_rewards = plot_data(data_x=data_x, data_y=avg_base_rewards, label_x='steps', label_y='rewards', label=f'{args.optim_base}', fig_no=1)
-
-    print(f"meta val: {avg_meta_func_vals[-1]:.4f}, base val: {avg_base_func_vals[-1]:.4f}")
-
-    print(f'saving plot to {meta_model_dir}/func_val_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
-    fig_func_val.savefig(f'{meta_model_dir}/func_val_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
-
-    print(f'saving plot to {meta_model_dir}/rewards_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
-    fig_rewards.savefig(f'{meta_model_dir}/rewards_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
+    if args.env == 'quadratic':
+        np.save(f'{meta_model_dir}/optimal_value.npy', env.f_opt)
     
+    optims = ['adam', 'sgd']
+    base_lrs = [1.0, 0.5, 0.1, 0.01, 0.001, 1e-4]
 
+    for opt in optims:
+        for lr in base_lrs:
+            env.reset()
+            env.cuda()
+            if opt == 'adam':
+                base_optimizer = optim.Adam(env.all_params, lr=lr, eps=1e-5)
+            elif opt == 'sgd':
+                base_optimizer = optim.SGD(env.all_params, lr=lr)
+            else:
+                raise NotImplementedError
+
+            base_func_vals, base_rewards = [], []
+            # import ipdb; ipdb.set_trace()
+            for step in trange(args.n_steps_test):
+                base_optimizer.zero_grad()
+                prev_func_vals = env.func_val
+                next_func_vals = env._eval()
+                base_optimizer.step()
+                env.func_val = next_func_vals
+                reward = prev_func_vals - next_func_vals
+                base_func_vals.append(_to_cpu(next_func_vals))
+                base_rewards.append(_to_cpu(reward))
+            base_func_vals = np.asarray(base_func_vals)
+            base_rewards = np.asarray(base_rewards)
+
+            avg_base_func_vals = np.mean(base_func_vals, axis=1)
+            avg_base_rewards = np.mean(base_rewards, axis=1)
+
+
+            std_base_func_vals = np.std(base_func_vals, axis=1)
+            std_base_rewards = np.std(base_rewards, axis=1)
+
+            print(f"saving test opt: {opt}, lr: {lr}")
+            np.save(f'{meta_model_dir}/avg_opt_{opt}_lr_{lr}_func_vals.npy', avg_base_func_vals)
+            np.save(f'{meta_model_dir}/std_opt_{opt}_lr_{lr}_func_vals.npy', std_base_func_vals)
+            np.save(f'{meta_model_dir}/opt_{opt}_lr_{lr}_func_vals.npy', np.transpose(base_func_vals))
+
+    # data_x = np.arange(args.n_steps_test)
+    # fig_func_val = plot_data(data_x=data_x, data_y=avg_meta_func_vals, label_x='steps', label_y='Func Val', label='meta', fig_no=0)
+    # fig_func_val = plot_data(data_x=data_x, data_y=avg_base_func_vals, label_x='steps', label_y='Func Val', label=f'{args.optim_base}', fig_no=0)
+
+
+    # fig_rewards = plot_data(data_x=data_x, data_y=avg_meta_rewards, label_x='steps', label_y='rewards', label='meta', fig_no=1)
+    # fig_rewards = plot_data(data_x=data_x, data_y=avg_base_rewards, label_x='steps', label_y='rewards', label=f'{args.optim_base}', fig_no=1)
+
+    # print(f"meta val: {avg_meta_func_vals[-1]:.4f}, base val: {avg_base_func_vals[-1]:.4f}")
+
+    # print(f'saving plot to {meta_model_dir}/func_val_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
+    # fig_func_val.savefig(f'{meta_model_dir}/func_val_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
+
+    # print(f'saving plot to {meta_model_dir}/rewards_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
+    # fig_rewards.savefig(f'{meta_model_dir}/rewards_{args.optim_base}_lr_{args.lr_base}_s_{args.n_steps_test}.png')
+    
 def main():
     
     meta_model_dir = f'{args.save_dir}/{args.env}/lr_{args.lr}_bs_{args.batch_size}_dim_{args.dimension}_hid_{args.hidden_size}_gamma_{args.gamma}_eps_{args.n_episodes}_steps_{args.n_steps}/'
